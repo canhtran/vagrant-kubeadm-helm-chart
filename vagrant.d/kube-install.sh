1723e8ddc7be431f7782e0caa025ef8c5b29f460
canhtran <trandcanh@gmail.com>
2018-10-11 18:26:19 +0800

    update kube scripts

--- vagrant.d/kube-install.sh
+++ vagrant.d/kube-install.sh
@@ -12,5 +12,19 @@ apt-mark hold kubelet kubeadm kubectl
 swapoff -a
 sudo sed-i '/ swap / s/^\(.*\)$/#\1/g' /etc/fstab
 
+sed -i '0,/ExecStart=/s//Environment="KUBELET_EXTRA_ARGS=--cgroup-driver=cgroupfs"\n&/' /etc/systemd/system/kubelet.service.d/10-kubeadm.conf
+
+IPADDR=`ifconfig enp0s8 | grep Mask | awk '{print $2}'| cut -f2 -d:`
+echo This VM has IP address $IPADDR
+
+# Set up Kubernetes
+NODENAME=$(hostname -s)
+kubeadm init --apiserver-cert-extra-sans=$IPADDR  --node-name $NODENAME
+
+# Set up admin creds for the vagrant user
+echo Copying credentials to /home/vagrant...
+sudo --user=vagrant mkdir -p /home/vagrant/.kube
+cp -i /etc/kubernetes/admin.conf /home/vagrant/.kube/config
+chown $(id -u vagrant):$(id -g vagrant) /home/vagrant/.kube/config
 
 
